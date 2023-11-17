63f498baf4a5207330da88b4e1a33009f5bd4763
andreiBe <andrei.be05@gmail.com>
2023-11-17 14:29:16 +0200

    Uusi hakemisto

--- hakemisto.js
+++
@@ -1,7 +0,0 @@
-const hakemisto = document.getElementById("hakemisto")
-
-const content = fetch("/hakemisto.html").then((content) => {
-    return content.text()
-}).then((text) => {
-    hakemisto.innerHTML = text;
-})
